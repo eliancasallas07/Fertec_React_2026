5d001e6e748ce4ae72b0e0768b84b7f5bd856b23
Elian Casallas <elian.casallas07@gmail.com>
2026-02-04 13:01:18 -0500

    Sidebar: enlaces organizados alfabéticamente

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,15 +4,16 @@ const Sidebar = ({ open, onClose }) => (
   <div className={`Sidebar${open ? ' open' : ''}`}>  
     <button className="Sidebar-close" onClick={onClose}>×</button>
     <ul className="Sidebar-list">
-      <li><Link to="/cerraduras_inteligentes" onClick={onClose}>Cerraduras Inteligentes</Link></li>
+      <li><Link to="/contactenos" onClick={onClose}>Contáctanos</Link></li>
+      <li><Link to="/cerraduras_bluetooth" onClick={onClose}>Cerraduras Bluetooth</Link></li>
+      <li><Link to="/cerraduras_codigo" onClick={onClose}>Cerraduras Código</Link></li>
       <li><Link to="/cerraduras_digitales" onClick={onClose}>Cerraduras Digitales</Link></li>
       <li><Link to="/cerraduras_huella" onClick={onClose}>Cerraduras Huella</Link></li>
-      <li><Link to="/cerraduras_bluetooth" onClick={onClose}>Cerraduras Bluetooth</Link></li>
+      <li><Link to="/cerraduras_inteligentes" onClick={onClose}>Cerraduras Inteligentes</Link></li>
+      <li><Link to="/cerraduras_manual" onClick={onClose}>Cerraduras Manual</Link></li>
       <li><Link to="/cerraduras_tarjeta" onClick={onClose}>Cerraduras Tarjeta</Link></li>
-      <li><Link to="/cerraduras_codigo" onClick={onClose}>Cerraduras Código</Link></li>
-      <li><Link to="/instaladores" onClick={onClose}>Directorio de Instaladores</Link></li>
       <li><Link to="/descarga_catalogos" onClick={onClose}>Descarga de Catálogos</Link></li>
-      <li><Link to="/contactenos" onClick={onClose}>Contáctanos</Link></li>
+      <li><Link to="/instaladores" onClick={onClose}>Directorio de Instaladores</Link></li>
     </ul>
   </div>
 );
